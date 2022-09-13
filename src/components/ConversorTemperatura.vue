627badae4eae7faf4f12ae8847710d0c07719eea
GIULIA LARISSA CONRADI <giulia.conradi@alunos.sc.senac.br>
2022-09-12 21:59:45 -0300

    conversor de temperatura

--- src/components/ConversorTemperatura.vue
+++ src/components/ConversorTemperatura.vue
@@ -1,10 +1,36 @@
 <template>
-  
+  <div>
+     <table>
+        <tr>
+          <th>Celsius</th>
+          <th>Fahrenheit</th>
+        </tr>
+        <tr>
+            <tr
+          v-for="p in projetosfiltrados"
+          :key="p.id"
+          @click="selected = p"
+          :class="{ selected: isSelected(p) }">
+        </tr>
+      </table>
+  </div>  
 </template>
 
 <script>
 export default {
+    props: {
+    c1: 20,
+    c2: 60,
+  },
+  data() {
+    return {
+      tabela: "",
+     mounted() {
+        this.projetos.forEach(p => maiorId = p.id > maiorId ? p.id : maiorId)
+     }    
+  }
 
+ }
 }
 </script>
 
